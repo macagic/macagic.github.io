4af263fa8ecf7ef6626f5978486e9fe34bd941f5
macagic <91361647+macagic@users.noreply.github.com>
2022-07-18 19:04:39 +0800

    Update 检查手机.js

--- Tweb/js/检查手机.js
+++ Tweb/js/检查手机.js
@@ -1,4 +1,5 @@
-  function IsPhone() {
+//   function IsPhone() {
+!function () {
       //获取浏览器navigator对象的userAgent属性（浏览器用于HTTP请求的用户代理头的值）
       var info = navigator.userAgent;
       //通过正则表达式的test方法判断是否包含“Mobile”字符串
